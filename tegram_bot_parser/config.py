7cb55c0f05d0eddaf9cca4799f340ed2d6f36d75
Pavel Sarancha <68758084+PaulLocust@users.noreply.github.com>
2022-02-12 22:09:54 +0300

    Delete tegram_bot_parser directory

--- tegram_bot_parser/config.py
+++
@@ -1,4 +0,0 @@
-URL = "https://www.computeruniverse.net/ru/c/apparatnoe-obespechenie-i-komponenty/videokarty-pci-express?page={}"
-TOKEN = '5153167520:AAGrqJ0sr3NUrOyRZTkq1DLQrjJii2vlxsU'
-OPERA_DRIVER_PATH = "C:\\Users\\Paul\\PycharmProjects\\parse_bot\\operadriver.exe"
-CHROME_DRIVER_PATH = "C:\\Users\\Paul\\PycharmProjects\\parse_bot\\chromedriver.exe"
